10969a08ef9a44f9d836b904d4fc6b1564c45f15
Win-Lai-Shwe-Yee <61868031+Win-Lai-Shwe-Yee@users.noreply.github.com>
2020-04-01 15:30:06 +0630

    Completed the Lesson3 TaskRunners

---
+++ WebBasic/Javascript/TaskRunners/gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+/*
+    -- TOP LEVEL FUNCTIONS --
+    gulp.task - Define tasks
+    gulp.src - Point to files to use
+    gulp.dest - Points to folder to output
+    gulp.watch - Watch files and folders for changes
+*/
+
+// Logs Message
+gulp.task('message', function(){
+    return console.log('Gulp is running...');
+});
+
+// Copy All HTML files
+gulp.task('copyHtml', function(){
+    gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+// Optimize Images
+gulp.task('imageMin', () => {
+    gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'));
+})
+
+// Minify JS
+gulp.task('minify', function(){
+    gulp.src('src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', gulp.parallel('message', 'copyHtml', 'imageMin', 'minify'));
---
+++ WebBasic/Javascript/TaskRunners/src/js/file1.js
@@ -0,0 +1,8 @@
+// Console Log 1
+console.log('This is log 1');
+
+
+
+
+// Console Log 2
+console.log('This is log 2');
